f007b25c285ece97b60faca51a3a2dbd307cf0a7
Catherine Phillips <catherineep4189@gmail.com>
2020-04-24 20:18:13 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-    if (array[i] === 1) {
+    if (array[i] === 0) {
       array.push('I am 1 strange loop.')
       } else {
         array.push(`I am ${i} strange loops.`)
